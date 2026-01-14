e72b0530d34ca5440db6108d9935565a36fdc305
Pawan Mehta <kpkumar.8757@gmail.com>
2026-01-14 20:18:19 +0530

    Time: 5 ms (59.27%), Space: 53.7 MB (60.06%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,37 +1,16 @@
 class Solution {
-
-    private int getNextGrayCode(int num, Set<Integer> allGrayCodeSt) {
-        for(int i = 0; i < 32; i++) {
-            int mask = 1 << i;
-            int newNum = num ^ mask;
-
-            if(!allGrayCodeSt.contains(newNum)) {
-                return newNum;
-            }
-        }
-        return -1;
+    private int numGrayCodeUtil(int n) {
+        return n ^ (n >> 1);
     }
 
     public List<Integer> grayCode(int n) {
-        Set<Integer> allGrayCodeSt = new HashSet<>();
-        List<Integer> allGrayCode = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
 
-        allGrayCode.add(0);
-        allGrayCodeSt.add(0);
-        allGrayCode.add(1);
-        allGrayCodeSt.add(1);
-
-        int num = 1;
-
-        while(allGrayCode.size() < ((int)Math.pow(2, n))) {
-            int nextGrayCodeNum = getNextGrayCode(num, allGrayCodeSt);
-
-            allGrayCode.add(nextGrayCodeNum);
-            allGrayCodeSt.add(nextGrayCodeNum);
-
-            num = nextGrayCodeNum;
+        int sequenceLength = 1 << n;
+        for(int i = 0; i < sequenceLength; i++) {
+            res.add(numGrayCodeUtil(i));
         }
 
-        return allGrayCode;
+        return res;
     }
 }
