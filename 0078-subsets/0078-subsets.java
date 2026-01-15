fcda23cffbf9c6092b31d2dfc6447a5f400e4023
Pawan Mehta <kpkumar.8757@gmail.com>
2026-01-15 16:07:58 +0530

    Time: 1 ms (88.09%), Space: 44 MB (80.01%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,29 +1,20 @@
 class Solution {
-    private Set<List<Integer>> subsetsSet;
-
-    private void subsetsUtil(int index, ArrayList<Integer> subset, int[] nums) {
-        if(index >= nums.length) {
-            subsetsSet.add(List.copyOf(subset));
-            return;
-        }
-
-        subset.add(nums[index]);
-        subsetsUtil(index + 1, subset, nums);
-        subset.remove(subset.size() - 1);
-
-        subsetsUtil(index + 1, subset, nums);
-    }
-    
     public List<List<Integer>> subsets(int[] nums) {
-        subsetsSet = new HashSet<>();
-
-        ArrayList<Integer> subset = new ArrayList<>();
-
-        subsetsUtil(0, subset, nums);
+        int n = nums.length;
 
         List<List<Integer>> subsetsRes = new ArrayList<>();
-        for(List<Integer> sb : subsetsSet) {
-            subsetsRes.add(sb);
+        
+        for(int num = 0; num < Math.pow(2, n); num++) {
+
+            ArrayList<Integer> subset = new ArrayList<>();
+
+            for(int i = 0; i < n; i++) {
+                if((num & (1 << i)) != 0) {
+                    subset.add(nums[i]);
+                }   
+            }
+
+            subsetsRes.add(subset);
         }
 
         return subsetsRes;
