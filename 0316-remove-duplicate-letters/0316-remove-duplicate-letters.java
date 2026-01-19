79df06ec1d93c69c84e858bf1f457f679a069a6d
Pawan Mehta <kpkumar.8757@gmail.com>
2026-01-20 00:39:20 +0530

    Time: 4 ms (21.72%), Space: 45 MB (6.1%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,24 +1,33 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] count = new int[26];
+        int n = s.length();
 
-        for(char ch : s.toCharArray()) {
-            count[ch - 'a']++;
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            lastOccurrence.put(s.charAt(i), i);
         }
 
-        int lastValidIndex = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) < s.charAt(lastValidIndex)) {
-                lastValidIndex = i;
-            }
+        Stack<Character> st = new Stack<>();
+        Set<Character> includedChar = new HashSet<>();
 
-            if(--count[s.charAt(i) - 'a'] == 0) {
-                break;
+        for(int i = 0; i < n; i++) {
+            if(!includedChar.contains(s.charAt(i))) {
+                while(!st.isEmpty() 
+                        && st.peek() > s.charAt(i)
+                        && lastOccurrence.get(st.peek()) > i) {
+                    includedChar.remove(st.pop());
+                }
+
+                st.push(s.charAt(i));
+                includedChar.add(s.charAt(i));
             }
         }
 
-        return s.length() == 0 ? "" : 
-            s.charAt(lastValidIndex) 
-                + removeDuplicateLetters(s.substring(lastValidIndex + 1).replaceAll("" + s.charAt(lastValidIndex), ""));
+        StringBuilder resStr = new StringBuilder();
+        while(!st.isEmpty()) {
+            resStr.append(st.pop());
+        }
+
+        return resStr.reverse().toString();
     }
 }
