f98e129f23851f8f6bc6e712b08bdbc5011a9590
Pawan Mehta <kpkumar.8757@gmail.com>
2026-01-25 17:53:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
@@ -1,12 +1,5 @@
 class Solution {
-    static int euclideanAlgo(int a, int b) {
-        if(a == 0) {
-            return b;
-        }
-        return euclideanAlgo(b%a, a);
-    }
-    
     public static int gcd(int a, int b) {
-        return euclideanAlgo(a, b);
+        return b == 0 ? a : gcd(b, a%b);
     }
 }
