3a344ecf8d4e07e751b9f7f7bf1c9746fdc9bbbd
Pawan Mehta <kpkumar.8757@gmail.com>
2026-01-31 17:12:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Search Pattern/search-pattern.java
+++ Difficulty: Hard/Search Pattern/search-pattern.java
@@ -1,47 +1,104 @@
-class Solution {
+
+class RabinKarp {
+    private final long mod1 = 1_000_000_007;
+    private final long mod2 = 1_000_000_009;
     
-    private int[] constructZArray(String str) {
-        int n = str.length();
-        
-        int[] zArray = new int[n];
-        
-        int l = 0, r = 0;
-        
-        for(int i = 1; i < n; i++) {
-            if(i <= r) {
-                int k = i - l;
-                zArray[i] = Math.min(zArray[k], r - i + 1);
-            }
-            
-            while((i + zArray[i]) < n && str.charAt(zArray[i]) == str.charAt(i + zArray[i])) {
-                zArray[i]++;
-            }
-            
-            if((i + zArray[i] - 1)  > r) {
-                l = i;
-                r = i + zArray[i] - 1;
-            }
-        }
-        
-        return zArray;
+    private final long base1 = 31;
+    private final long base2 = 37;
+    
+    private long[] hash1;
+    private long[] hash2;
+    
+    private long[] pow1;
+    private long[] pow2;
+    
+    
+    public RabinKarp(String str) {
+        calculateHash(str);
     }
     
+    private long charToLong(char ch) {
+        return (long)(ch - 'a' + 1);    
+    }
+    
+    private long add(long a, long b, long mod) {
+        a += b;
+        if(a >= mod) {
+            a -= mod;
+        }
+        return a;
+    }
+    
+    private long sub(long a, long b, long mod) {
+        a -= b;
+        if(a < 0) {
+            a += mod;
+        }
+        return a;
+    }
+    
+    private long multi(long a, long b, long mod) {
+        return (a * b) % mod;
+    }
+    
+    private void calculateHash(String str) {
+        int n = str.length();
+        
+        hash1 = new long[n];
+        hash2 = new long[n];
+        
+        pow1 = new long[n];
+        pow2 = new long[n];
+        
+        hash1[0] = charToLong(str.charAt(0));
+        hash2[0] = charToLong(str.charAt(0));
+        
+        pow1[0] = 1;
+        pow2[0] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            hash1[i] = add(multi(hash1[i - 1], base1, mod1), charToLong(str.charAt(i)), mod1);
+            pow1[i] = multi(pow1[i - 1], base1, mod1);
+            
+            hash2[i] = add(multi(hash2[i - 1], base2, mod2), charToLong(str.charAt(i)), mod2);
+            pow2[i] = multi(pow2[i - 1], base2, mod2);
+        }
+    }
+    
+    public long[] getHash(int l, int r) {
+        long hash1Val = hash1[r];
+        long hash2Val = hash2[r];
+        
+        if(l > 0) {
+            hash1Val = sub(hash1Val, multi(hash1[l - 1], pow1[r - l + 1], mod1), mod1);
+            hash2Val = sub(hash2Val, multi(hash2[l - 1], pow2[r - l + 1], mod2), mod2);
+        }
+        
+        return new long[]{hash1Val, hash2Val}; 
+    }
+}
+
+class Solution {
     ArrayList<Integer> search(String pat, String txt) {
         int n = txt.length();
         int m = pat.length();
         
-        String str = pat + '$' + txt;
+        RabinKarp txtHash = new RabinKarp(txt);
+        RabinKarp patHash = new RabinKarp(pat);
         
-        int[] zArray = constructZArray(str);
+        long[] patHashValues = patHash.getHash(0, m - 1);
         
-        ArrayList<Integer> resIndx = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
         
-        for(int i = m + 1; i < zArray.length ; i++) {
-            if(zArray[i] == pat.length()) {
-                resIndx.add(i - m - 1);
+        for(int i = 0; i <= (n - m); i++) {
+            long[] substrHashValues = txtHash.getHash(i, i + m - 1);
+            
+            if(patHashValues[0] == substrHashValues[0] && 
+                    patHashValues[1] == substrHashValues[1]) {
+                res.add(i);            
             }
         }
         
-        return resIndx;
+        return res;
     }
 }
