c1d84e477c89379be21200008fe4ccf6ce0f1c0f
Pawan Mehta <kpkumar.8757@gmail.com>
2026-02-10 23:25:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,34 +1,20 @@
 class Solution {
     
-    private int knapsackUtil(int i, int W, int[] val, int[] wt, int[][] memo) {
-        if(i >= val.length) {
-            return 0;
-        }
-        
-        if(memo[i][W] != -1) {
-            return memo[i][W];
-        }
-        
-        int opt1 = 0, opt2 = 0;
-        
-        if(wt[i] <= W) {
-            opt1 = val[i] + knapsackUtil(i + 1, W - wt[i], val, wt, memo);
-        }
-        
-        opt2 = knapsackUtil(i + 1, W, val, wt, memo);
-        
-        return memo[i][W] = Math.max(opt1, opt2);
-    }
-    
     public int knapsack(int W, int val[], int wt[]) {
         int n = val.length;
         
-        int[][] memo = new int[n][W + 1];
+        int[][] dp = new int[n + 1][W + 1];
         
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= W; j++) {
+                if(wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
         
-        return knapsackUtil(0, W, val, wt, memo);
+        return dp[n][W];
     }
 }
