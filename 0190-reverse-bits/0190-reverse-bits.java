d80db13cdd2bb47483527773e934d8b3c8957dc5
Pawan Mehta <kpkumar.8757@gmail.com>
2026-02-16 21:24:12 +0530

    Time: 0 ms (100%), Space: 42.4 MB (85.32%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,13 +1,14 @@
 class Solution {
     public int reverseBits(int n) {
-        int revBitNum = 0;
-        for(int i = 0, j = 31; i < 32; i++, j--) {
+        int revNum = 0;
+        
+        for(int i = 0, j = 31; j >= 0; i++, j--) {
             int mask = 1 << i;
             if((n & mask) != 0) {
-                int rMask = 1 << j;
-                revBitNum = revBitNum | rMask;
+                revNum = revNum | (1 << j);
             }
         }
-        return revBitNum;
+
+        return revNum;
     }
 }
