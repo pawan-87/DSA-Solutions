cc3e1e52a689a584340b4e4264a466cc1693c0d3
Pawan Mehta <kpkumar.8757@gmail.com>
2026-02-18 01:53:55 +0530

    Time: 5 ms (83.74%), Space: 49.7 MB (13.77%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,62 +1,15 @@
 class Solution {
-
-    private Set<String> permutations;
-
-    private String getTimeStr(int hours, int minutes) {
-        int hr = hours;
-        int mn = minutes;
-
-        StringBuilder time = new StringBuilder();
-
-        time.append(hr);
-        time.append(":");
-
-        if(mn <= 9) {
-            time.append(0);
-        }
-
-        time.append(mn);
-
-        return time.toString();
-    }
-
-    private void readBinaryWatchPermutation(int hours, int minutes, int remainingLights) {
-        if(hours > 11 || minutes > 59) {
-            return;
-        }
-
-        if(remainingLights == 0) {
-            permutations.add(getTimeStr(hours, minutes));
-            return;
-        }
-
-        int mask = 0;
-
-        for(int i = 0; i < 4; i++) {
-            mask = 1 << i;
-            if((hours & mask) == 0) {
-                readBinaryWatchPermutation(hours | mask, minutes, remainingLights - 1);
-            }
-        }
-
-        for(int i = 0; i < 6; i++) {
-            mask = 1 << i;
-            if((minutes & mask) == 0) {
-                readBinaryWatchPermutation(hours, minutes | mask, remainingLights - 1);
-            }
-        }
-    }
-
     public List<String> readBinaryWatch(int turnedOn) {
-        permutations = new HashSet<>();
+        List<String> list = new ArrayList<>();
 
-        readBinaryWatchPermutation(0, 0, turnedOn);
-
-        List<String> res = new ArrayList<>();
-        for(String time : permutations) {
-            res.add(time);
+        for(int hr = 0; hr <= 11; hr++) {
+            for(int mn = 0; mn <= 59; mn++) {
+                if(Integer.bitCount(hr) + Integer.bitCount(mn) == turnedOn) {
+                    list.add(hr + ":" + (mn <= 9 ? "0" + mn : mn));
+                }
+            }
         }
 
-        return res;
+        return list;
     }
 }
