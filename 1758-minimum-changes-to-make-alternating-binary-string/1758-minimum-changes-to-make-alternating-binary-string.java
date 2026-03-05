d14b2fa4b4bc5208e40b0deaffa07648ff38a381
Pawan Mehta <kpkumar.8757@gmail.com>
2026-03-05 09:23:16 +0530

    Time: 3 ms (92.43%), Space: 43.7 MB (79.17%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -2,36 +2,24 @@ class Solution {
     public int minOperations(String s) {
         int n = s.length();
 
-        int operation1 = 0;
-        for(int i = 1; i < n; i += 2) {
-            if(!(s.charAt(i - 1) == '0' && s.charAt(i) == '1')) {
-                if(s.charAt(i - 1) != '0') {
-                    operation1++;
+        int start0 = 0, start1 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(i%2 == 0) {
+                if(s.charAt(i) == '0') {
+                    start1++;
+                } else {
+                    start0++;
                 }
-                if(s.charAt(i) != '1') {
-                    operation1++;
+            } else {
+                if(s.charAt(i) == '1') {
+                    start1++;
+                } else {
+                    start0++;
                 }
             }
         }
-        if(n%2 != 0 && s.charAt(n - 1) != '0') {
-            operation1++;
-        }
 
-        int operation2 = 0;
-        for(int i = 1; i < n; i += 2) {
-            if(!(s.charAt(i - 1) == '1' && s.charAt(i) == '0')) {
-                if(s.charAt(i - 1) != '1') {
-                    operation2++;
-                }
-                if(s.charAt(i) != '0') {
-                    operation2++;
-                }
-            }
-        }
-        if(n%2 != 0 && s.charAt(n - 1) != '1') {
-            operation2++;
-        }
-
-        return Math.min(operation1, operation2);
+        return Math.min(start0, start1);
     }
 }
